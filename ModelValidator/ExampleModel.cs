171d4d8a37e6f41ab6cd278795b57c56707c6c19
Alexander <zam.kuratora@gmail.com>
2020-07-16 02:53:41 +0300

    initial commit

---
+++ ModelValidator/ExampleModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace $rootnamespace$.Models
+{
+    /// <summary>
+    /// Example model.
+    /// </summary>
+    public class ExampleModel : ModelValidatorBase
+    {
+        /// <summary>
+        /// Example string.
+        /// </summary>
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Input text")]
+        [StringLength(maximumLength: 10, MinimumLength = 4, ErrorMessage = "Min length: {1}, max length: {0}")]
+        public string ExampleString { get; set; }
+
+        /// <summary>
+        /// Example int.
+        /// </summary>
+        [Range(minimum: 5, maximum: 10, ErrorMessage = "Min value: {0}, max value: {1}")]
+        public int ExampleInt { get; set; }
+    }
+}
---
+++ ModelValidator/ModelValidatorBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace $rootnamespace$.Models
+{
+    /// <summary>
+    /// Model validator.
+    /// </summary>
+    public abstract class ModelValidatorBase : IDataErrorInfo
+    {
+        /// <summary>
+        /// Current type.
+        /// </summary>
+        Type currentType;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public ModelValidatorBase()
+        {
+            currentType = GetType();
+        }
+
+        /// <summary>
+        /// Gets property value by name.
+        /// </summary>
+        /// <param name="propName">Property name.</param>
+        /// <returns></returns>
+        private object GetPropertyValue(string propName) => currentType.GetProperty(propName).GetValue(this);
+
+        /// <summary>
+        /// All errors in model.
+        /// </summary>
+        public string Error => string.Join(Environment.NewLine, GetValidationErrors());
+
+        /// <summary>
+        /// Returns valid status.
+        /// </summary>
+        public bool IsValid => !GetValidationErrors().Any();
+
+        /// <summary>
+        /// Gets error text by property name.
+        /// </summary>
+        /// <param name="propName">Property name.</param>
+        /// <returns></returns>
+        public string this[string propName] => GetValidationError(propName);
+
+        /// <summary>
+        /// Gets error text by property name.
+        /// </summary>
+        /// <param name="propertyName">Property name.</param>
+        /// <returns></returns>
+        protected string GetValidationError(string propertyName)
+        {
+            string error = string.Empty;
+            var context = new ValidationContext(this) { MemberName = propertyName };
+            var results = new List<ValidationResult>();
+
+            if (!Validator.TryValidateProperty(GetPropertyValue(propertyName), context, results))
+            {
+                error = results.First().ErrorMessage;
+            }
+
+            return error;
+        }
+
+        /// <summary>
+        /// Returns all errors in model.
+        /// </summary>
+        protected IEnumerable<ValidationResult> GetValidationErrors()
+        {
+            var context = new ValidationContext(this);
+            var results = new List<ValidationResult>();
+
+            Validator.TryValidateObject(this, context, results, true);
+
+            return results;
+        }
+    }
+}
